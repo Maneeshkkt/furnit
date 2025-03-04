537f022950cbf910d89ebb692a1a4a68ac6a85d4
Maneeshkkt <141389961+Maneeshkkt@users.noreply.github.com>
2025-03-04 11:25:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('active');
     navbg.classList.toggle('active');
 });
+
+
+let index = 0;
+                const slides = document.querySelectorAll(".slide");
+                const slider = document.getElementById("slider");
+                
+                function showSlide(n) {
+                    if (n >= slides.length) index = 0;
+                    else if (n < 0) index = slides.length - 1;
+                    else index = n;
+                    slider.style.transform = `translateX(${-index * 100}%)`;
+                }
+                function nextSlide() { showSlide(index + 1); }
+                function prevSlide() { showSlide(index - 1); }
+                setInterval(() => nextSlide(), 3000);
