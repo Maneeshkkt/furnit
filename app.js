70aa48b825c6b4f4db85982f5b907dbe44366612
Maneeshkkt <141389961+Maneeshkkt@users.noreply.github.com>
2025-03-19 18:30:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,26 @@ let index = 0;
                 function nextSlide() { showSlide(index + 1); }
                 function prevSlide() { showSlide(index - 1); }
                 setInterval(() => nextSlide(), 3000);
+
+
+//   counting figure              
+
+// Get allValues using querySelectorAll
+let allValues = document.querySelectorAll(".value");
+
+// Start the forEach loop for displaying the values
+allValues.forEach((singleValue) => {
+  let startValue = 0;
+  let endValue = parseInt(singleValue.getAttribute("data-value"));
+  let duration = Math.floor(2000 / endValue);
+
+  // Counter for increaing the values & display
+  let counter = setInterval(function () {
+    startValue += 1;
+    singleValue.textContent = startValue;
+    // Clearing the interval
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
